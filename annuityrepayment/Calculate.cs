c3cbdf3d58ea58be67f208050c18f3b94b65efa3
Johannes Losch <hi@johanneslosch.com>
2020-03-20 21:05:44 +0100

    added calculation

--- annuityrepayment/Calculate.cs
+++ annuityrepayment/Calculate.cs
@@ -10,50 +10,64 @@ namespace annuityrepayment
     {
         public static void calculate(double __tilgung, double __zinssatz, double __darlehn)
         {
-           
+            int jahr = 1;
+            double saldo;
+            double tilgung;
+            double zinsen;
+            double annuitaet = 0;
+            double restschuld = 0;
+
             //TODO: clear file after each run
             double __repayment = (__darlehn * __zinssatz) + (__darlehn * __tilgung);
-             writeToFile("Year 1 - darlehn after: " + (__darlehn - __repayment));
-            //double y2 = __darlehn - __repayment - __repayment;
-            //writeToFile("Year 2 - darlehn after: " + y2);
-            int year = 2;
-            //
-            while(__repayment > 0)
-            {
-                Console.WriteLine(year);
-                double y2 = __darlehn - __repayment - __repayment;
-                __repayment = y2;
-                writeToFile("Year " + year + ": " + y2);
-                year++;
-            }
-
-            /**
+            jahr = 1;
+        
+            tilgung = __darlehn * __tilgung;
+            zinsen = __darlehn * __zinssatz;
+            annuitaet = tilgung + zinsen;
+            restschuld = __darlehn - tilgung;
+            writeToFile("Jahr " + jahr + "; Saldo: " + __darlehn + " €; Tilgung: " + tilgung + " €; Zinsen: " + zinsen + " €; Restschuld: " + restschuld + " €");
             do
             {
-                /*calc second
+                Console.Write("\n");
+                Console.Write("----------");
+                /*calc second*/
                 saldo = restschuld;
-                zinsen = saldo * zinssatz / 100;
+                zinsen = saldo * __zinssatz;
                 tilgung = annuitaet - zinsen;
                 restschuld = saldo - tilgung;
                 jahr++;
-                /*output
-                cout << endl << "Jahr: " << jahr;
-                cout << endl << "Saldo: " << saldo;
-                cout << endl << "Tilgung: " << tilgung;
-                cout << endl << "Zinsen: " << zinsen;
-                cout << endl << "Annuitaet: " << annuitaet;
-                cout << endl << "Restschuld: " << restschuld;
-
+                /*output*/
+                writeToFile("Jahr " + jahr + "; Saldo: " + saldo + " €; Tilgung: " + tilgung + " €; Zinsen: " + zinsen + " €; Restschuld: " + restschuld + " €");
+                Console.Write("\n");
+                Console.Write("Jahr: ");
+                Console.Write(jahr);
+                Console.Write("\n");
+                Console.Write("Saldo: ");
+                Console.Write(saldo);
+                Console.Write("\n");
+                Console.Write("Tilgung: ");
+                Console.Write(tilgung);
+                Console.Write("\n");
+                Console.Write("Zinsen: ");
+                Console.Write(zinsen);
+                Console.Write("\n");
+                Console.Write("Annuitaet: ");
+                Console.Write(annuitaet);
+                Console.Write("\n");
+                Console.Write("Restschuld: ");
+                Console.Write(restschuld);
             } while (restschuld > 0);
-            */
 
-            Console.WriteLine("tilgung " + __tilgung + " Zinssatz " + __zinssatz + "/ Darlehn " + __darlehn + "\n" + __repayment);
+
+
+                Console.WriteLine("tilgung " + __tilgung + " Zinssatz " + __zinssatz + "/ Darlehn " + __darlehn + "\n" + __repayment);
        }
 
         private static void writeToFile(String content)
         {
-            File.AppendAllText(@"../../test.txt", content + Environment.NewLine);
+            File.AppendAllText(@"../../test1.txt", content + Environment.NewLine);
 
         }
     }
 }
+
